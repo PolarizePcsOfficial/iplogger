55b780d67ec18d619770f7ef3a81e8ce444a62d6
PolarizePcsOfficial <123495627+PolarizePcsOfficial@users.noreply.github.com>
2024-10-01 22:11:54 -0600

    Update log-ip.js

--- api/log-ip.js
+++ api/log-ip.js
@@ -4,22 +4,25 @@ const axios = require('axios');
 
 module.exports = async (req, res) => {
     try {
+        // Fetch the public IP from an external service
         const response = await axios.get('https://api.ipify.org?format=json');
         const ipAddress = response.data.ip;
 
-        fs.readFile(path.join(__dirname, '../ips.json'), (err, data) => {
+        // Read the current IPs from ips.json
+        fs.readFile(path.join(__dirname, '../ips.json'), 'utf8', (err, data) => {
             let ips = [];
-            if (!err) {
+            if (!err && data) {
                 ips = JSON.parse(data);
             }
-            ips.push({ ip: ipAddress, date: new Date() });
+            ips.push({ ip: ipAddress, date: new Date() }); // Append the new IP
 
+            // Write the updated IPs back to ips.json
             fs.writeFile(path.join(__dirname, '../ips.json'), JSON.stringify(ips, null, 2), (err) => {
                 if (err) {
                     console.error('Error saving IP address:', err);
                     return res.status(500).send('Error saving IP address.');
                 }
-                res.json({ ip: ipAddress });
+                res.json({ ip: ipAddress }); // Send it back as a JSON response
             });
         });
     } catch (error) {
